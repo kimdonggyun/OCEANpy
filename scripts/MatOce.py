dcbaea1af9e5ab45d65c747ffd2c39fedae3a7e4
kimdonggyun <kdk921219@gmail.com>
2020-10-16 10:10:30 +0200

    new for temp

--- scripts/MatOce.py
+++ scripts/MatOce.py
@@ -1,1 +1,15 @@
 # calculation for any oceanographic formula
+
+def Cel_to_Kel (Cel):
+    # convert Celsius to Kelvin
+    Kel = Cel + 273.15
+    return Kel
+
+def Kel_to_Cel (Kel):
+    # convert Kelvin to Celsius
+    Cel = Kel - 273.15
+    return Cel
+
+def to_pot_temp ():
+    # convert temperature to potential temperature
+    pass
