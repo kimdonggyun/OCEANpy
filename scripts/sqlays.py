7aad1ab5a20e8983fff200419747ec4affb47147
Dong-gyun Kim <dkim@template.awi.de>
2020-10-23 13:59:57 +0200

    awi server psql done

--- scripts/sqlays.py
+++ scripts/sqlays.py
@@ -5,14 +5,19 @@ import pandas as pd
 import sqlalchemy, os
 from ctdays import raw_ctd_to_df
 
-def export_sql(database_name, table_name, location):
+def export_sql(database_name, edit, location):
     # export table from sql
     if location == 'local':
         mydb = pgsql.connect(dbname='%s'%(database_name,), host='localhost', user='dong', password='Lava10203!')
+        cur = mydb.cursor()
+        df = pd.read_sql('''SELECT * FROM %s'''%(edit), mydb)
     elif location == 'awi_server':
         mydb = pgsql.connect(dbname='%s'%(database_name,), host='postgres5.awi.de', user='loki', password='DwnmdN!')
-    cur = mydb.cursor()
-    df = pd.read_sql('''SELECT * FROM %s'''%(table_name), mydb)
+        cur = mydb.cursor()
+        df = pd.read_sql('''SELECT * 
+                        FROM vessel v, cruise c, station s, loki_data l, taxa_group t 
+                        WHERE c.id_vessel = v.id_vessel AND c.id_cruise = s.id_cruise AND s.id_station = l.id_station AND l.id_taxa_group = t.id_taxa_group
+                        AND c.name ILIKE ANY ( ARRAY %s ) '''%(edit,), mydb)
     return df
 
 def import_sql (database_name,table_name, df, replace_or_append):
@@ -26,7 +31,8 @@ def import_sql (database_name,table_name, df, replace_or_append):
 if __name__ == "__main__":
     action = 'export'
     if action == 'export':
-        df = export_sql('loki', 'cruise', 'awi_server')
+        edit = ['ps107'] # cruise name (in list, case insensitive) for AWI server and table name for local
+        df = export_sql('loki', edit, 'awi_server')
         print(df)
       
     elif action == 'import':
