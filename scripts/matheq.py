cf5b89056e309cd1384217c4acd2dbb277ac4d8c
Dong-gyun Kim <dkim@rz-00365.dmawi.de>
2020-10-30 18:20:52 +0100

    dist hist

--- scripts/matheq.py
+++ scripts/matheq.py
@@ -1,4 +1,5 @@
 # calculation for any oceanographic formula
+import math
 
 def Cel_to_Kel (Cel):
     # convert Celsius to Kelvin
@@ -13,3 +14,7 @@ def Kel_to_Cel (Kel):
 def to_pot_temp ():
     # convert temperature to potential temperature
     pass
+
+def calDist (x1, y1, x2, y2):
+    dist = math.sqrt( (x2-x1)**2 + (y2-y1)**2  )
+    return dist
