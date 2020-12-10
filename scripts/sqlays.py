04e77388612c0d29aaf0efc3ba63cfd38d7f5f98
Dong-gyun Kim <dong-gyun.kim@awi.de>
2020-12-10 10:39:05 +0100

    sql edit

--- scripts/sqlays.py
+++ scripts/sqlays.py
@@ -13,7 +13,7 @@ def export_sql(database_name, edit, location):
         mydb = pgsql.connect(dbname='%s'%(database_name,), host='localhost', user='dong', password='Lava10203!')
         cur = mydb.cursor()
         df = pd.read_sql('''SELECT * FROM %s'''%(edit), mydb)
-    elif location == 'awi_server':
+    elif (location == 'awi_server') & (edit != 'all'):
         mydb = pgsql.connect(dbname='%s'%(database_name,), host='postgres5.awi.de', user='loki', password='DwnmdN!')
         cur = mydb.cursor()
         
@@ -29,6 +29,23 @@ def export_sql(database_name, edit, location):
                         WHERE c.id_vessel = v.id_vessel AND c.id_cruise = s.id_cruise 
                         AND s.id_station = l.id_station AND l.id_taxa_group = t.id_taxa_group
                         AND c.name ILIKE ANY ( ARRAY %s ) ORDER BY s.name, l.loki_depth  '''%(edit,), mydb)
+        
+    elif (location == 'awi_server') & (edit == 'all'):
+        mydb = pgsql.connect(dbname='%s'%(database_name,), host='postgres5.awi.de', user='loki', password='DwnmdN!')
+        cur = mydb.cursor()
+        
+        df = pd.read_sql('''SELECT v.name as vessel, c.name as cruise, 
+                        s.name as station, s.region as region, s.latitude as latitude, s.longitude as longitude, s.bottom_depth as bottom_depth,
+                        l.datetime as date_time,
+                        l.loki_depth as loki_depth, l.temperature as temperatue, l.salinity as salinity, l.oxygen_conc as oxygen, l.fluorescence as fluorescence,
+                        l.manual_classification as manual_classification, l.developmental_stage as developmental_stage, l.area_pixel as area_pixel,
+                        l.area_sqrmm as area_sqrmm, l.spec_length as spec_length, l.spec_width as spec_width, l.image_filename as image_filename,
+                        t.animal as animal, t.copepod as copepod, t.phylum as phylum, t.class as class, t.spec_order as spec_order,
+                        t.family as family, t.genus as genus, t.species as species
+                        FROM vessel v, cruise c, station s, loki_data l, taxa_group t 
+                        WHERE c.id_vessel = v.id_vessel AND c.id_cruise = s.id_cruise 
+                        AND s.id_station = l.id_station AND l.id_taxa_group = t.id_taxa_group
+                        ORDER BY s.name, l.loki_depth  ''', mydb)
     return df
 
 def import_sql (database_name,table_name, df, replace_or_append):
@@ -37,7 +54,3 @@ def import_sql (database_name,table_name, df, replace_or_append):
     df.to_sql('%s' % (table_name,), engine, if_exists=replace_or_append, index=False, 
             dtype={col_name: sqlalchemy.types.String() for col_name in df})
     
-
-'''
-
-'''
