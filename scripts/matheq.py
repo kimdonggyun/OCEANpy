ba5933e33f2b832765dd96754a1d4b17504dea20
Dong-gyun Kim <dong-gyun.kim@awi.de>
2021-03-29 11:38:47 +0200

    oxycom

--- scripts/matheq.py
+++ scripts/matheq.py
@@ -16,5 +16,19 @@ def to_pot_temp ():
     pass
 
 def calDist (x1, y1, x2, y2):
+    # calculate the distance between to point
     dist = math.sqrt( (x2-x1)**2 + (y2-y1)**2  )
     return dist
+
+def calOxyconsume (DW, Temp):
+    # calculate oxygen consumption rate
+    # DW: dry weight of individual taxa
+    # Temperature
+    oxycon = math.e**(-0.399 + 0.801*math.log(DW) + 0.069*math.log(Temp))
+    return oxycon
+
+def calOxycomsume2Carbon ():
+    
+    return carbon
+    
+    
